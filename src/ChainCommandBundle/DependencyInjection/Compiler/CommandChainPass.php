335eb2e870a75048172a793ec4758d30c8442f0b
Mykola Zyk <relo.san.pub@gmail.com>
2016-02-08 08:54:13 +0200

    Rewrite checking empty member-of attribute in chain pass.

--- src/ChainCommandBundle/DependencyInjection/Compiler/CommandChainPass.php
+++ src/ChainCommandBundle/DependencyInjection/Compiler/CommandChainPass.php
@@ -24,27 +24,22 @@ class CommandChainPass implements CompilerPassInterface
     {
         $chainServices = $container->findTaggedServiceIds('console.chain');
 
-        $members = [];
         $chains = [];
 
         foreach ($chainServices as $id => $tags) {
             $definition = $container->getDefinition($id);
             $commandClass = $container->getParameterBag()->resolveValue($definition->getClass());
 
-
             foreach ($tags as $tag) {
-                if (!empty($tag['member-of'])) {
-                    $mainCommand = $tag['member-of'];
-                    $members[$commandClass][] = $mainCommand;
-                    $chains[$mainCommand][] = $commandClass;
+                if (empty($tag['member-of'])) {
+                    throw new \InvalidArgumentException(sprintf(
+                        'Tagged command chaining "%s" must have configured "member-of" attribute.',
+                        $id
+                    ));
                 }
-            }
 
-            if (!isset($members[$commandClass])) {
-                throw new \InvalidArgumentException(sprintf(
-                    'Tagged command chaining "%s" must have configured "member-of" attribute.',
-                    $id
-                ));
+                $mainCommand = $tag['member-of'];
+                $chains[$mainCommand][] = $commandClass;
             }
         }
 
